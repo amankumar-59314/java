885d0ca3579d23e8f2c280321342241f1a1b2183
amankumar-59314 <amankumar59314@gmail.com>
2023-09-23 16:46:00 +0530

    done

---
+++ Javabascis.java
@@ -0,0 +1,17 @@
+public class Javabascis {
+    public static void main(String args[]) {
+        // System.out.print("HeLlo World");
+        // System.out.print("HeLlo World");
+        // System.out.print("HeLlo World");
+        // System.out.println("HeLlo World");//line ka space
+        // System.out.println("HeLlo World");//line ka space
+        // System.out.println("HeLlo World");//line ka space
+        // System.out.print("HeLlo World\n");//line ka space
+        // System.out.print("HeLlo World\n");//line ka space
+        // System.out.print("HeLlo World\n");//line ka space
+        System.out.print("HeLlo World\nHello world");//line ka space
+        System.out.print("HeLlo World\nHello world");//line ka space
+        System.out.print("HeLlo World\nHello world");//line ka space
+    }
+}
+//boilerplate code
