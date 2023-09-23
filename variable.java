8ff916b0d3c1dcaa8445b8964416d805ca303dc0
amankumar-59314 <amankumar59314@gmail.com>
2023-09-23 21:02:50 +0530

    first commit

---
+++ variable.java
@@ -0,0 +1,19 @@
+public class variable {
+    public static void main(String args[]){
+        int a = 10;
+        int b = 5;
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(a+b*a);
+        String name ="Aman Choudhary";
+        System.out.println(name);
+
+
+        a=100;
+        System.out.println(a);
+
+
+        a=b;
+        System.out.println(a);
+    }
+    }
