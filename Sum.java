a01416481c85f9222edd6e88ff85176b754d7c0d
amankumar-59314 <amankumar59314@gmail.com>
2023-09-25 07:00:32 +0530

    https://github.com/amankumar-59314/java.git

---
+++ Sum.java
@@ -0,0 +1,23 @@
+public class Sum {
+    public static void main (String args[]){
+        int a = 10;
+        int b = 5;
+        int sum = a+b;
+        System.out.println(sum);
+        //Sub
+        // int a= 10 ;
+        // int b = 5;
+        // int sub =a-b;
+        // System.out.println(sub);
+       /*mul
+       int a= 10;
+       int b=5;
+        int mul =a*b;
+      System.out.println(mul);*/
+        //Div
+    //    int a = 10;
+    //    int b = 5;
+    //    int div = a/b;
+    //    System.out.println(div);
+    }
+    }
